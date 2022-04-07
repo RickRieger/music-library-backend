74828f56977e379fa463123f69c2354f80d083ab
Rick Rieger <rickrieger36251@gmail.com>
2022-04-07 15:06:17 -0400

    added DELETE functionality to API

--- music/views.py
+++ music/views.py
@@ -18,7 +18,7 @@ def songs_list(request):
     return Response(serializer.data, status=status.HTTP_201_CREATED)  
 
 
-@api_view(['GET', 'PUT'])
+@api_view(['GET', 'PUT', 'DELETE'])
 def songs_detail(request, pk):
   song = get_object_or_404(Song, pk=pk)
   if request.method == 'GET': 
@@ -28,4 +28,7 @@ def songs_detail(request, pk):
     serializer = SongSerializer(song, data=request.data)
     serializer.is_valid(raise_exception=True)
     serializer.save()
-    return Response(serializer.data)   
+    return Response(serializer.data)
+  elif request.method == 'DELETE':
+      song.delete()
+      return Response(status=status.HTTP_200_OK)     
