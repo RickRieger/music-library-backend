4e9523b4b51f4919bc14889deaef7b97d9ce29c2
Rick Rieger <rickrieger36251@gmail.com>
2022-04-07 14:30:51 -0400

    Added get-all-songs functionality to API with a response code 200

--- music/serializers.py
+++ music/serializers.py
@@ -1,7 +1,7 @@
 from rest_framework import serializers
 from .models import Song
 
-class SongSerilaizer(serializers.ModelSerializer):
+class SongSerializer(serializers.ModelSerializer):
   class Meta: 
     model = Song
     fields = ['id', 'title', 'artist', 'album', 'release_date', 'genre', 'like_count', 'dislike_count']
--- music/views.py
+++ music/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from .serializers import SongSerializer
+from .models import Song 
 
-# Create your views here.
+@api_view(['GET'])
+def songs_list(request):
+  if request.method == 'GET':
+    songs = Song.objects.all()
+    serializer = SongSerializer(songs, many=True)
+    return Response(serializer.data,status=status.HTTP_200_OK)
+
+
+@api_view(['PUT'])
+def songs_detail(request, pk):
+  return
